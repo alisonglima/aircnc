3c0c93663d2cd955bec039339ec241a9b6b16d1a
Alison Gustavo de Lima <alisonlima2@hotmail.com>
2020-04-23 20:22:21 -0300

    Início da implementação do frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
 const routes = require("./routes");
 
 const app = express();
@@ -11,6 +13,7 @@ mongoose.connect(process.env.MONGO_URL, {
   useUnifiedTopology: true,
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
