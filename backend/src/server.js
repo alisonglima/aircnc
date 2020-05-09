61e387cd8a5ba87ae654fcb17f5f4c51c8b3b207
Alison Gustavo de Lima <alisonlima2@hotmail.com>
2020-05-09 12:27:32 -0300

    Finalização da implementação do frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 const routes = require("./routes");
 
@@ -15,6 +16,7 @@ mongoose.connect(process.env.MONGO_URL, {
 
 app.use(cors());
 app.use(express.json());
+app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
 app.listen(process.env.PORT || 3333);
