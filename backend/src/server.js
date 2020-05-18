8c8d4e946d9f3cf1c34ecc0dc7289c417a000397
Alison Gustavo de Lima <alisonlima2@hotmail.com>
2020-05-18 17:00:04 -0300

    Finalização da implementação do mobile e websockets

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,13 +5,33 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 
+const socketio = require("socket.io");
+const http = require("http");
+
 const routes = require("./routes");
 
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
+
+const connectedUsers = {};
 
 mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+});
+
+io.on("connection", (socket) => {
+  const { user_id } = socket.handshake.query;
+
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
 });
 
 app.use(cors());
@@ -19,4 +39,4 @@ app.use(express.json());
 app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
-app.listen(process.env.PORT || 3333);
+server.listen(process.env.PORT || 3333);
